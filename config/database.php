850f53f8d12c860ad2d827272918284437a24b48
ihebsaad <ihebsaad@gmail.com>
2020-05-01 10:38:51 +0100

    add composer

--- config/database.php
+++ config/database.php
@@ -44,7 +44,7 @@ return [
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'almanahel'),
-            'username' => env('DB_USERNAME', 'manahel'),
+            'username' => env('DB_USERNAME', 'manaheluser'),
             'password' => env('DB_PASSWORD', 'eSolutions2020*'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',

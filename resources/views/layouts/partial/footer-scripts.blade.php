3156a675cb2da63d6eb5a9a3e49c087cfe3c37d3
ihebsaad <ihebsaad@gmail.com>
2020-05-04 14:30:20 +0100

    adding layouts

---
+++ resources/views/layouts/partial/footer-scripts.blade.php
@@ -0,0 +1,32 @@
+ 
+
+<!--<script src="{{--  URL::asset('public/js/jquery-ui/jquery.ui.min.js') --}}" type="text/javascript"></script>-->
+{{ csrf_field() }}
+
+<script src="{{  URL::asset('public/js/bootstrap.min.js') }}" type="text/javascript"></script>
+<script src="{{  URL::asset('public/js/custom_js/metisMenu.js') }}" type="text/javascript"></script>
+<script src="{{  URL::asset('public/js/jquery-slimscroll/js/jquery.slimscroll.min.js') }}" type="text/javascript"></script>
+<script src="{{  URL::asset('public/js/custom_js/owl.carousel.min.js') }}" type="text/javascript"></script>
+<script src="{{  URL::asset('public/js/custom_js/datetime.js') }}" type="text/javascript"></script>
+<script src="{{  URL::asset('public/js/jstree/jstree.min.js') }}" type="text/javascript"></script>
+
+ <!--<script src="{{  URL::asset('public/js/custom_js/fixed_layout.js') }}" type="text/javascript"></script>-->
+ 
+<script  src="{{ asset('public/js/summernote.min.js') }}"  type="text/javascript"></script>
+<script src="{{  URL::asset('public/js/custom_js/compose.js') }}" type="text/javascript"></script>
+ 
+<!----- Datepicker ------->
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+ 
+
+ 
+<!-- end of page level js -->
+<!-- begin page level js -->
+@yield('footer_scripts')
+<script type="text/javascript">
+ 
+
+</script>
+

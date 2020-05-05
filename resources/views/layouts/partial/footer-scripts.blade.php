92f5c20c44c9f9f9b8d6ea961ed052752ce6354c
ihebsaad <ihebsaad@gmail.com>
2020-05-05 10:52:27 +0100

    coorect script

--- resources/views/layouts/partial/footer-scripts.blade.php
+++ resources/views/layouts/partial/footer-scripts.blade.php
@@ -2,8 +2,8 @@
 {{ csrf_field() }}
 
    <!-- Bootstrap core JavaScript -->
-  <script    src="{{  URL::asset('public/site/js/jquery.min.js') }}"   ></script>
-  <script    src="{{  URL::asset('public/site/js/bootstrap.bundle.min.js') }}"     ></script>
+  <script    src="{{  URL::asset('public/site/vendor/jquery/jquery.min.js') }}"   ></script>
+  <script    src="{{  URL::asset('public/site/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"     ></script>
 
   <!-- Custom scripts for this template -->
   <script  src="{{  URL::asset('public/site/js/clean-blog.min.js') }}"  ></script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
   <!-- Page Header -->
-  <header class="masthead" style= "background-image:{{ asset('public/site/img/home-bg.jpg') }}">
+  <header class="masthead" style= "background-image:url(`{{ asset('public/site/img/home-bg.jpg') }}`)" >
     <div class="overlay"></div>
     <div class="container">
       <div class="row">

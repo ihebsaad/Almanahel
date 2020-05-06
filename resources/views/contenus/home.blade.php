e09a2044732ab0054fc737f0919eadd11c2e0a00
ihebsaad <ihebsaad@gmail.com>
2020-05-06 15:54:57 +0100

    fix home

--- resources/views/contenus/home.blade.php
+++ resources/views/contenus/home.blade.php
@@ -1,12 +1,17 @@
 @extends('layouts.back')
 
 @section('content')
-    <link href="{{ asset('public/css/summernote.css') }}" rel="stylesheet" media="screen" />
+<!-- include summernote css/js -->
+<link href="https://cdn.jsdelivr.net/npm/summernote@0.8.16/dist/summernote.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/summernote@0.8.16/dist/summernote.min.js"></script>
 
 <h1>Gestion du contenu: Page d'Accueil </h1>
 <div class="form-group">
      {{ csrf_field() }}
+<?php 
+$cont =  App\Contenu::where('zone', 'contact')->first();$contenu=$cont->contenu ;
 
+?>
 <div class="modal-body">
     <form id="updateform">
 
@@ -15,7 +20,7 @@
        <div class="form-group ">
                     <label for="contenu">Contenu:</label>
                     <div class="editor" >
-                        <textarea style="min-height: 380px;"  id="home" type="text"  class="textarea tex-com" placeholder="Contenu ici" name="home" required  ></textarea>
+                        <textarea style="min-height: 380px;"  id="home" type="text"  class="textarea tex-com" placeholder="Contenu ici" name="home" required  ><?php echo $contenu; ?></textarea>
                     </div>
          </div>
 
@@ -33,67 +38,28 @@
   </div>
 
 @endsection
-
-<!--
-<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
--->
-
 <script>
 
-    function changing(elm) {
-        var champ=elm.id;
-
-        var val =document.getElementById(champ).value;
-        //  var type = $('#type').val();
-        var citie = $('#id').val();
-         //if ( (val != '')) {
+   function changing(elm) {
+        
+        var val =document.getElementById(elm).value;
+      
         var _token = $('input[name="_token"]').val();
         $.ajax({
-            url: "{{ route('actualites.updating') }}",
+            url: "{{ route('home.updatecontent') }}",
             method: "POST",
-            data: {citie: citie , champ:champ ,val:val, _token: _token},
+            data: {zone: elm   ,val:val, _token: _token},
             success: function (data) {
-                $('#'+champ).animate({
+                $('#'+elm).animate({
                     opacity: '0.3',
                 });
-                $('#'+champ).animate({
+                $('#'+elm).animate({
                     opacity: '1',
                 });
 
             }
         });
-        // } else {
-
-        // }
-    }
-
-    function disabling(elm) {
-        var champ=elm;
-
-        var val =1;
-         var citie = $('#id').val();
-        //if ( (val != '')) {
-        var _token = $('input[name="_token"]').val();
-        $.ajax({
-            url: "{{ route('actualites.updating') }}",
-            method: "POST",
-            data: {actualite: actualite , champ:champ ,val:val, _token: _token},
-            success: function (data) {
-                if (elm=='annule'){
-                $('#nonactif').animate({
-                    opacity: '0.3',
-                });
-                $('#nonactif').animate({
-                    opacity: '1',
-                });
-                }
-
-
-            }
-        });
-        // } else {
-
-        // }
+       
     }
 
 </script>

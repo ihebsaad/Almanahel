84c5b67cbcca7e91c533ae67cdf3fca2f6c69408
ihebsaad <ihebsaad@gmail.com>
2020-05-06 16:25:07 +0100

    test

--- resources/views/users/view.blade.php
+++ resources/views/users/view.blade.php
@@ -148,9 +148,6 @@
         </style>
 
 
-
-<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-
 <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
 <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
 

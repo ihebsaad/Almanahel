408ec5b75dd3713bf7456b0582de1d941cef7d66
ihebsaad <ihebsaad@gmail.com>
2020-05-06 15:59:58 +0100

    test

--- resources/views/actualites/index.blade.php
+++ resources/views/actualites/index.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.back')
-<!--
+ 
 <link rel="stylesheet" type="text/css" href="{{ asset('resources/assets/datatables/css/dataTables.bootstrap.css') }}" />
 <link rel="stylesheet" type="text/css" href="{{ asset('resources/assets/datatables/css/buttons.bootstrap.css') }}" />
 
 <link rel="stylesheet" type="text/css" href="{{ asset('resources/assets/datatables/css/scroller.bootstrap.css') }}" />
--->
+ 
 
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"  >
 

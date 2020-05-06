35ead43fb18c8a2c25a4c8b037b4ff06cd13d959
ihebsaad <ihebsaad@gmail.com>
2020-05-06 16:34:09 +0100

    test

--- resources/views/users/view.blade.php
+++ resources/views/users/view.blade.php
@@ -4,9 +4,10 @@
 <link href="{{ asset('public/js/select2/css/select2-bootstrap.css') }}" rel="stylesheet" type="text/css"/>
 
 
+@section('content')
+
 <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
 <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
-@section('content')
     <div class=" " style="padding:8px 8px 8px 8px">
         <div class="portlet box grey">
             <h3>   Utilisateur</h3>

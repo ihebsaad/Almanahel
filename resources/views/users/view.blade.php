1fac16e3cf4f38a6b6d6ac9ad9f0967d07df440f
ihebsaad <ihebsaad@gmail.com>
2020-05-06 16:30:36 +0100

    test fix select2

--- resources/views/users/view.blade.php
+++ resources/views/users/view.blade.php
@@ -148,9 +148,6 @@
         </style>
 
 
-<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
-<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
-
 <script>
 
 $(function () {

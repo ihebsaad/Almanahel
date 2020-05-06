b342a27fb0522aa39f3dfa7fdc00460b07bb6640
ihebsaad <ihebsaad@gmail.com>
2020-05-06 10:54:34 +0100

    test

--- resources/views/contenus/contact.blade.php
+++ resources/views/contenus/contact.blade.php
@@ -1,9 +1,17 @@
 @extends('layouts.back')
 
+
+<!-- include libraries(jQuery, bootstrap) -->
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<!-- include summernote css/js -->
+<link href="https://cdn.jsdelivr.net/npm/summernote@0.8.16/dist/summernote.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/summernote@0.8.16/dist/summernote.min.js"></script>
 @section('content')
 
-    <link href="{{ asset('public/css/summernote.css') }}" rel="stylesheet" media="screen" />
-
+ 
 <h1>Gestion du contenu: Page Contact </h1>
 <div class="form-group">
      {{ csrf_field() }}

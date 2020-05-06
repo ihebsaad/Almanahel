beb87e5a2af83fb2c0ff72449f570e814498020a
ihebsaad <ihebsaad@gmail.com>
2020-05-06 10:47:35 +0100

    test

--- public/js/custom_js/compose.js
+++ public/js/custom_js/compose.js
@@ -8,8 +8,12 @@
               placeholder: 'Votre contenu ici...',
 
               fontNames: ['Open Sans','Arial','Courier New'],
-              height:300
-
+              height:300,
+			popover: {
+			image: [],
+			link: [],
+			air: []
+			}
           });
 		  $('.textarea:first').summernote('focus');
 

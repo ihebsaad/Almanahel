ed1d574b7432b1d6c9a690f571a1acc5e4f675f1
haythemsahlia <haithemsahlia@gmail.com>
2020-05-07 09:45:00 +0100

    fix web file

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,20 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
-Auth::routes();
 
+Route::get('/home', array('as' => 'home','uses' => 'HomeController@home'));
+Route::get('/presentation', array('as' => 'presentation','uses' => 'HomeController@presentation'));
+Route::get('/formation', array('as' => 'formation','uses' => 'HomeController@formation'));
+Route::get('/scolaire', array('as' => 'scolaire','uses' => 'HomeController@scolaire'));
+Route::get('/inscription', array('as' => 'inscription','uses' => 'HomeController@inscription'));
+Route::get('/contact', array('as' => 'contact','uses' => 'HomeController@contact'));
+
+Route::post('/updatecontent', array('as' => 'home.updatecontent','uses' => 'HomeController@updatecontent'));
+
+Auth::routes();
 
 
 
@@ -50,7 +59,13 @@ Route::get('/actualites', array('as' => 'actualites','uses' => 'ActualitesContro
 Route::post('/actualites/saving','ActualitesController@saving')->name('actualites.saving');
 Route::post('/actualites/updating','ActualitesController@updating')->name('actualites.updating');
 Route::get('/actualites/view/{id}', 'ActualitesController@view');
-/*** users **/
+Route::get('/actualites/destroy/{id}', 'ActualitesController@destroy');
+ 
+ 
+ 
+ /*** Users **/
+
+//Route::resource('/users',  'UsersController');
 Route::get('/users', array('as' => 'users','uses' => 'UsersController@index'));
 Route::get('/users/create','UsersController@create')->name('users.create');
 Route::post('/users/saving','UsersController@saving')->name('users.saving');
@@ -60,15 +75,19 @@ Route::get('/users/profile/{id}', 'UsersController@profile')->name('profile');
 Route::post('/users/createuserrole', 'UsersController@createuserrole')->name('users.createuserrole');
 Route::post('/users/removeuserrole', 'UsersController@removeuserrole')->name('users.removeuserrole');
 Route::post('/users/sessionroles', 'UsersController@sessionroles')->name('users.sessionroles');
-Route::post('/changestatut', 'UsersController@changestatut')->name('users.changestatut');
 Route::post('/users/createeleve','UsersController@createeleve')->name('users.createeleve');
 Route::post('/users/removeeleve','UsersController@removeeleve')->name('users.removeeleve');
+Route::post('/changestatut', 'UsersController@changestatut')->name('users.changestatut');
 Route::get('/users/destroy/{id}', 'UsersController@destroy');
-//Route::get('/edit/{id}','UsersController@edit');
 Route::post('/edit/{id}','UsersController@update');
 
+/****  Contenus  *****/
+
+Route::get('/contenuhome', array('as' => 'contenuhome','uses' => 'HomeController@contenu_home'));
+Route::get('/contenupresentation', array('as' => 'contenupresentation','uses' => 'HomeController@contenu_presentation'));
+Route::get('/contenuformation', array('as' => 'contenuformation','uses' => 'HomeController@contenu_formation'));
+Route::get('/contenuscolaire', array('as' => 'contenuscolaire','uses' => 'HomeController@contenu_scolaire'));
+Route::get('/contenucontact', array('as' => 'contenucontact','uses' => 'HomeController@contenu_contact'));
+Route::get('/contenuinscription', array('as' => 'contenuinscription','uses' => 'HomeController@contenu_inscription'));
 
 
-
-
- 

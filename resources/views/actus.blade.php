50c1637a3e042e042bc4ff41a9ace26120a752b6
ihebsaad <ihebsaad@gmail.com>
2020-05-08 10:04:05 +0100

    actus

--- resources/views/actus.blade.php
+++ resources/views/actus.blade.php
@@ -1,4 +1,4 @@
- <!--
+ 
     <div class="row">
       <div class="col-lg-8 col-md-10 mx-auto">
         <div class="post-preview">
@@ -59,7 +59,7 @@
         </div>
       </div>
     </div>
- --->
+ 
  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>

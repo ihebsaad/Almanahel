5b7c23b84d03f04fe65947070a37aaa2f2bf1756
ihebsaad <ihebsaad@gmail.com>
2020-05-08 10:11:21 +0100

    test

--- resources/views/actus.blade.php
+++ resources/views/actus.blade.php
@@ -60,7 +60,9 @@
       </div>
     </div>
  --->
- 
+ <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
  
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.css">

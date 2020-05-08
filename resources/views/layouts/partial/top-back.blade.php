ba1bc803822f91fb8ea3f57899433a1c0ebf7d55
ihebsaad <ihebsaad@gmail.com>
2020-05-08 09:58:53 +0100

    update

--- resources/views/layouts/partial/top-back.blade.php
+++ resources/views/layouts/partial/top-back.blade.php
@@ -167,7 +167,7 @@
             <li class="nav-item dropdown no-arrow">
               <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <?php  $user = auth()->user();  ?>
-				<span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo $user->name.' '.$user->lastname ;?></span>
+				<span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo $user['name'].' '.$user['lastname'] ;?></span>
                 <img class="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60">
               </a>
               <!-- Dropdown - User Information -->

aa66e7b630ced13eb863caf93af8c2f4a39b0bf2
haythemsahlia <haithemsahlia@gmail.com>
2020-05-08 14:06:47 +0100

    footer front

--- resources/views/layouts/partial/head.blade.php
+++ resources/views/layouts/partial/head.blade.php
@@ -8,6 +8,8 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
+  <link rel="shortcut icon" type="image/png" href="{{  URL::asset('public/site/img/favicon.png') }}">
+
   <title>Al Manahel Academy </title>
     <?php    setlocale(LC_ALL, "fr_FR.UTF-8");    ?>
   <!-- Bootstrap core CSS -->
@@ -18,34 +20,14 @@
   <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
   <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 
-  <link rel="stylesheet" href="{{  URL::asset('public/site/vendor/fontawesome-free/css/font-awesome.min.css') }}">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css">
 
   <!-- Custom styles for this template -->
   <link  href="{{  URL::asset('public/site/css/clean-blog.min.css') }}"   rel="stylesheet">
   <link  href="{{  URL::asset('public/site/css/slicknav.min.css') }}"   rel="stylesheet">
   <link  href="{{  URL::asset('public/site/css/menu.css') }}"   rel="stylesheet">
+  <link  href="{{  URL::asset('public/site/css/custom.css') }}"   rel="stylesheet">
 
-  <style type="text/css">
-  body { font-family: 'Roboto';}
-  @media screen and (max-width: 40em) {
-    
-
-   #menumob {
-        display:block!important;
-        position: absolute;
-        right: 0px;
-        z-index: 100;
-    }
-
-    #sticky-header {padding: 10px 0px;}
-
-    .slicknav_menu {
-    background: #7f8085; }
-    .slicknav_btn {
-    background-color: #7f8085;}
-
-}
-</style>
 
 </head>
 
@@ -55,18 +37,16 @@
 <div class="header-top bg-2">
 <div class="container">
 <div class="row">
-<div class="col-md-6 col-sm-8 col-xs-12">
+<div class="col-md-6 col-sm-6 col-xs-6">
 <div class="header-top-left">
 <p>LYCÉE Al-MANAHEL MONASTIR</p>
 </div>
 </div>
-<div class="col-md-6 col-sm-4 col-xs-12">
+<div class="col-md-6 col-sm-6 col-xs-6">
 <div class="header-top-right text-right">
 <ul>
-<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+<li><a href="https://www.facebook.com/almanahel.monastir/" target="blank"><i class="fa fa-facebook"></i></a></li>
 <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
 <li><a href="#"><i class="fa fa-youtube"></i></a></li>
 </ul>
 </div>

0ab444643b4de296f67ec9ca96de502d1e7cc2db
ihebsaad <ihebsaad@gmail.com>
2020-05-08 10:33:57 +0100

    logout

--- resources/views/layouts/partial/top-back.blade.php
+++ resources/views/layouts/partial/top-back.blade.php
@@ -187,7 +187,7 @@
                 <div class="dropdown-divider"></div>
                 <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                   <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Logout
+                  Déconnexion
                 </a>
               </div>
             </li>

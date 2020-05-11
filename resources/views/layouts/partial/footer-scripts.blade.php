5ed7468a800931c6e48f66fc57c69cc6cc10ece5
ihebsaad <ihebsaad@gmail.com>
2020-05-11 10:21:39 +0100

    test

--- resources/views/layouts/partial/footer-scripts.blade.php
+++ resources/views/layouts/partial/footer-scripts.blade.php
@@ -5,28 +5,7 @@
   <script    src="{{  URL::asset('public/site/vendor/jquery/jquery.min.js') }}"   ></script>
   <script    src="{{  URL::asset('public/site/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"     ></script>
 
-  <!-- Custom scripts for this template -->
-  <script  src="{{  URL::asset('public/site/js/clean-blog.min.js') }}"  ></script>
-<script  src="{{  URL::asset('public/site/js/jquery.slicknav.min.js') }}"  ></script>
-<script type="text/javascript">
-	$('ul#navigation').slicknav({
-		prependTo:"#menumob"
-	});
-	//$('#navigation').slicknav();
-	
-  $(function() {
-    $("#news-slider10").owlCarousel({
-        items : 4,
-        itemsDesktop:[1199,3],
-        itemsDesktopSmall:[980,2],
-        itemsMobile : [600,1],
-        navigation:true,
-        navigationText:["",""],
-        pagination:true,
-        autoPlay:true
-    });
-			});
-</script>
+
 <!-- google map -->
 <script>
 // Initialize and add the map

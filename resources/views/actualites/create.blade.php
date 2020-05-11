d9dd86749849114aab7864fefc1261a9837ee22c
ihebsaad <ihebsaad@gmail.com>
2020-05-11 14:38:08 +0100

    test

--- resources/views/actualites/create.blade.php
+++ resources/views/actualites/create.blade.php
@@ -1,6 +1,10 @@
 
 @extends('layouts.back')
 
+
+<link href="https://cdn.jsdelivr.net/npm/summernote@0.8.16/dist/summernote.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/summernote@0.8.16/dist/summernote.min.js"></script>
+
 @section('content')
     <style>
         .uper {

a319938638ce21aa04d41ed8a642b992538734c4
ihebsaad <ihebsaad@gmail.com>
2020-05-11 10:29:47 +0100

    test css

--- resources/views/actus.blade.php
+++ resources/views/actus.blade.php
@@ -1,162 +1,70 @@
-<!--
-    <div class="row">
-      <div class="col-lg-8 col-md-10 mx-auto">
-        <div class="post-preview">
-          <a href="post.html">
-            <h2 class="post-title">
-              Man must explore, and this is exploration at its greatest
-            </h2>
-            <h3 class="post-subtitle">
-              Problems look mighty small from 150 miles up
-            </h3>
-          </a>
-          <p class="post-meta">Posted by
-            <a href="#">Start Bootstrap</a>
-            on September 24, 2019</p>
-        </div>
-        <hr>
-        <div class="post-preview">
-          <a href="post.html">
-            <h2 class="post-title">
-              I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.
-            </h2>
-          </a>
-          <p class="post-meta">Posted by
-            <a href="#">Start Bootstrap</a>
-            on September 18, 2019</p>
-        </div>
-        <hr>
-        <div class="post-preview">
-          <a href="post.html">
-            <h2 class="post-title">
-              Science has not yet mastered prophecy
-            </h2>
-            <h3 class="post-subtitle">
-              We predict too much for the next year and yet far too little for the next ten.
-            </h3>
-          </a>
-          <p class="post-meta">Posted by
-            <a href="#">Start Bootstrap</a>
-            on August 24, 2019</p>
-        </div>
-        <hr>
-        <div class="post-preview">
-          <a href="post.html">
-            <h2 class="post-title">
-              Failure is not an option
-            </h2>
-            <h3 class="post-subtitle">
-              Many say exploration is part of our destiny, but it’s actually our duty to future generations.
-            </h3>
-          </a>
-          <p class="post-meta">Posted by
-            <a href="#">Start Bootstrap</a>
-            on July 8, 2019</p>
-        </div>
-        <hr>
-         <div class="clearfix">
-          <a class="btn btn-primary float-right" href="#">Older Posts &rarr;</a>
-        </div>
-      </div>
-    </div>
- --> <!------
- <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
  
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.theme.min.css">
-    
-	 
- <div class="container">
-    <h3 class="h3">New slider Demo - 10</h3>
-    <div class="row">
-        <div class="col-md-12">
-            <div id="news-slider10" class="owl-carousel">
-                <div class="post-slide10">
-                    <img src="http://bestjquery.com/tutorial/news-slider/demo18/images/img-1.jpg" alt="">
-                    <div class="post-date">
-                        <span class="month">Nov</span>
-                        <span class="date">5</span>
-                    </div>
-                    <h3 class="post-title">
-                        <a href="#">Lorem ipsum dolor sit amet, consectetur.</a>
-                    </h3>
-                    <p class="post-description">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam consectetur cumque dolorum, ex incidunt ipsa laudantium necessitatibus neque quae tempora......
-                    </p>
-                    <a href="#" class="read-more">read more<i class="fa fa-chevron-right"></i></a>
-                </div>
- 
-                <div class="post-slide10">
-                    <img src="http://bestjquery.com/tutorial/news-slider/demo18/images/img-2.jpg" alt="">
-                    <div class="post-date">
-                        <span class="month">Nov</span>
-                        <span class="date">8</span>
-                    </div>
-                    <h3 class="post-title">
-                        <a href="#">Lorem ipsum dolor sit amet, consectetur.</a>
-                    </h3>
-                    <p class="post-description">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam consectetur cumque dolorum, ex incidunt ipsa laudantium necessitatibus neque quae tempora......
-                    </p>
-                    <a href="#" class="read-more">read more<i class="fa fa-chevron-right"></i></a>
-                </div>
-                
-                <div class="post-slide10">
-                    <img src="http://bestjquery.com/tutorial/news-slider/demo18/images/img-3.jpg" alt="">
-                    <div class="post-date">
-                        <span class="month">Nov</span>
-                        <span class="date">8</span>
-                    </div>
-                    <h3 class="post-title">
-                        <a href="#">Lorem ipsum dolor sit amet, consectetur.</a>
-                    </h3>
-                    <p class="post-description">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam consectetur cumque dolorum, ex incidunt ipsa laudantium necessitatibus neque quae tempora......
-                    </p>
-                    <a href="#" class="read-more">read more<i class="fa fa-chevron-right"></i></a>
-                </div>
-                
-                <div class="post-slide10">
-                    <img src="http://bestjquery.com/tutorial/news-slider/demo18/images/img-4.jpg" alt="">
-                    <div class="post-date">
-                        <span class="month">Nov</span>
-                        <span class="date">8</span>
-                    </div>
-                    <h3 class="post-title">
-                        <a href="#">Lorem ipsum dolor sit amet, consectetur.</a>
-                    </h3>
-                    <p class="post-description">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam consectetur cumque dolorum, ex incidunt ipsa laudantium necessitatibus neque quae tempora......
-                    </p>
-                    <a href="#" class="read-more">read more<i class="fa fa-chevron-right"></i></a>
-                </div>
-                
-                <div class="post-slide10">
-                    <img src="http://bestjquery.com/tutorial/news-slider/demo18/images/img-5.jpg" alt="">
-                    <div class="post-date">
-                        <span class="month">Nov</span>
-                        <span class="date">8</span>
-                    </div>
-                    <h3 class="post-title">
-                        <a href="#">Lorem ipsum dolor sit amet, consectetur.</a>
-                    </h3>
-                    <p class="post-description">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam consectetur cumque dolorum, ex incidunt ipsa laudantium necessitatibus neque quae tempora......
-                    </p>
-                    <a href="#" class="read-more">read more<i class="fa fa-chevron-right"></i></a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<hr> -->
-
 <style>
 .card-body .btn-primary{color:white!important;}
 .carousel-item .active {display:table-row;}
+.controls-top	{
+		box-sizing:border-box;
+color:rgb(255, 255, 255);
+cursor:pointer;
+display:inline-block;
+font-family:"Font Awesome 5 Free";
+font-size:20px;
+font-style:normal;
+font-variant-caps:normal;
+font-variant-east-asian:normal;
+font-variant-ligatures:normal;
+font-variant-numeric:normal;
+font-weight:900;
+height:47px;
+line-height:47px;
+text-align:center;
+text-rendering:auto;
+text-size-adjust:100%;
+user-select:none;
+width:47px;
+-webkit-font-smoothing:antialiased;
+	}
+ .carousel-indicators{
+background-clip:padding-box;
+background-color:rgb(66, 133, 244);
+border-bottom-color:rgba(0, 0, 0, 0);
+border-bottom-left-radius:50%;
+border-bottom-right-radius:50%;
+border-bottom-style:solid;
+border-bottom-width:10px;
+border-top-color:rgba(0, 0, 0, 0);
+border-top-left-radius:50%;
+border-top-right-radius:50%;
+border-top-style:solid;
+border-top-width:10px;
+box-sizing:content-box;
+color:rgb(33, 37, 41);
+cursor:pointer;
+display:list-item;
+flex-basis:auto;
+flex-grow:0;
+flex-shrink:1;
+font-family:Roboto, sans-serif;
+font-size:16px;
+font-weight:300;
+height:20px;
+line-height:24px;
+list-style-image:none;
+list-style-position:outside;
+list-style-type:none;
+margin-bottom:-60px;
+margin-left:3px;
+margin-right:3px;
+max-width:20px;
+opacity:0.5;
+text-align:center;
+text-indent:-999px;
+text-size-adjust:100%;
+transition-delay:0s;
+transition-duration:0.6s;
+transition-property:opacity;
+transition-timing-function:ease;
+width:20px;}
 </style>
 
 <!--Carousel Wrapper-->

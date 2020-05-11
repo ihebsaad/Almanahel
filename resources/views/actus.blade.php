dc2c75a70c4ae540adf4979d2885852134447d2f
ihebsaad <ihebsaad@gmail.com>
2020-05-11 10:26:12 +0100

    test

--- resources/views/actus.blade.php
+++ resources/views/actus.blade.php
@@ -177,7 +177,7 @@
   <div class="carousel-inner" role="listbox">
 
     <!--First slide-->
-    <div class="carousel-item active">
+    <div class="row  carousel-item active">
 
       <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
         <div class="card mb-2">
@@ -188,7 +188,7 @@
             <h4 class="card-title">Card title</h4>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
               card's content.</p>
-            <a class="btn btn-primary">Button</a>
+            <a class="btn btn-primary">Lire Plus</a>
           </div>
         </div>
       </div>
@@ -202,7 +202,7 @@
             <h4 class="card-title">Card title</h4>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
               card's content.</p>
-            <a class="btn btn-primary">Button</a>
+            <a class="btn btn-primary">Lire Plus</a>
           </div>
         </div>
       </div>
@@ -216,7 +216,7 @@
             <h4 class="card-title">Card title</h4>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
               card's content.</p>
-            <a class="btn btn-primary">Button</a>
+            <a class="btn btn-primary">Lire Plus</a>
           </div>
         </div>
       </div>
@@ -225,7 +225,7 @@
     <!--/.First slide-->
 
     <!--Second slide-->
-    <div class="carousel-item">
+    <div class="row carousel-item">
 
       <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
         <div class="card mb-2">
@@ -235,7 +235,7 @@
             <h4 class="card-title">Card title</h4>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
               card's content.</p>
-            <a class="btn btn-primary">Button</a>
+            <a class="btn btn-primary">Lire Plus</a>
           </div>
         </div>
       </div>
@@ -248,7 +248,7 @@
             <h4 class="card-title">Card title</h4>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
               card's content.</p>
-            <a class="btn btn-primary">Button</a>
+            <a class="btn btn-primary">Lire Plus</a>
           </div>
         </div>
       </div>
@@ -261,7 +261,7 @@
             <h4 class="card-title">Card title</h4>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
               card's content.</p>
-            <a class="btn btn-primary">Button</a>
+            <a class="btn btn-primary">Lire Plus</a>
           </div>
         </div>
       </div>
@@ -270,7 +270,7 @@
     <!--/.Second slide-->
 
     <!--Third slide-->
-    <div class="carousel-item">
+    <div class="row carousel-item">
 
       <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
         <div class="card mb-2">
@@ -280,7 +280,7 @@
             <h4 class="card-title">Card title</h4>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
               card's content.</p>
-            <a class="btn btn-primary">Button</a>
+            <a class="btn btn-primary">Lire Plus</a>
           </div>
         </div>
       </div>
@@ -293,7 +293,7 @@
             <h4 class="card-title">Card title</h4>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
               card's content.</p>
-            <a class="btn btn-primary">Button</a>
+            <a class="btn btn-primary">Lire Plus</a>
           </div>
         </div>
       </div>
@@ -306,7 +306,7 @@
             <h4 class="card-title">Card title</h4>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
               card's content.</p>
-            <a class="btn btn-primary">Button</a>
+            <a class="btn btn-primary">Lire Plus</a>
           </div>
         </div>
       </div>

751008ed9043ecafae272eb88b0681a1ddf3ff8d
ihebsaad <ihebsaad@gmail.com>
2020-05-11 16:32:05 +0100

    test

--- resources/views/actualites/index.blade.php
+++ resources/views/actualites/index.blade.php
@@ -45,14 +45,14 @@
    
 
                 <tr>
-                    <td  >{{$actualite->id}}</td>
-                    <td  ><a href="{{action('ActualitesController@view', $actualite['id'])}}" ><center><?php if($actualite->image==''){ ?> <img src="{{  URL::asset('public/site/img/no-image.png') }}" style="width:100px" /> <?php }else{  ?><img src="http://<?php echo $_SERVER['HTTP_HOST'];?>/storage/images/<?php echo $actualite->image;?>" style="max-width:150px"/> <?php } ?></center> </a></td>
-                    <td  ><a href="{{action('ActualitesController@view', $actualite['id'])}}" >{{$actualite->titre}}</a></td>
-                    <td  > {{$actualite->titre}} </td>
-                    <td  >   <label><span class="checked">
+                    <td style="width:5%" >{{$actualite->id}}</td>
+                    <td style="width:20%" ><a href="{{action('ActualitesController@view', $actualite['id'])}}" ><center><?php if($actualite->image==''){ ?> <img src="{{  URL::asset('public/site/img/no-image.png') }}" style="width:100px" /> <?php }else{  ?><img src="http://<?php echo $_SERVER['HTTP_HOST'];?>/storage/images/<?php echo $actualite->image;?>" style="max-width:150px"/> <?php } ?></center> </a></td>
+                    <td style="width:20%" ><a href="{{action('ActualitesController@view', $actualite['id'])}}" >{{$actualite->titre}}</a></td>
+                    <td style="width:35%" > {{$actualite->titre}} </td>
+                    <td style="width:10%" >   <label><span class="checked">
                             <input  class="actus-<?php echo $actualite->id;?>"  type="checkbox"    id="actus-<?php echo $actualite->id;?>"    <?php if ($actualite->visible ==1){echo 'checked'; }  ?>  onclick="changing(this,'<?php echo $actualite->id; ?>' );"      >
                         </span> Visible</label> </td>
-					<td    >
+					<td style="width:10%"   >
                         @can('isAdmin')
                             <a  onclick="return confirm('Êtes-vous sûrs ?')" href="{{action('ActualitesController@destroy', $actualite['id'])}}" class="btn btn-danger btn-sm btn-responsive " role="button" data-toggle="tooltip" data-tooltip="tooltip" data-placement="bottom" data-original-title="Supprimer" >
                                 <span class="fa fa-fw fa-trash-alt"></span> Supprimer

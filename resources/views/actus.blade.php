f9ab88ce4e102b0ad2cdf03748dec302618f0015
ihebsaad <ihebsaad@gmail.com>
2020-05-13 10:58:36 +0100

    test

--- resources/views/actus.blade.php
+++ resources/views/actus.blade.php
@@ -145,8 +145,8 @@ $actualites=array();
        	 <?php if($image==''){ ?> <img  class="card-img-top" src="{{  URL::asset('public/site/img/no-image.png') }}" style="width:300px!important;height:219px" /> <?php }else{  ?><img src="http://<?php echo $_SERVER['HTTP_HOST'];?>/storage/images/<?php echo $image;?>" style="width:320px;height:219px"/><?php } ?>
 
           <div class="card-body">
-            <h4 class="card-title"><?php echo $titre; ?></h4>
-            <p class="card-text"> <?php custom_echo($contenu, 20)  ; ?> </p><br>
+            <h4 class="card-title"><?php custom_echo($contenu, 20) ?></h4>
+            <p class="card-text"> <?php custom_echo($contenu, 30)  ; ?> </p><br>
             <a class="link btn btn-primary"  href="{{action('ActualitesController@view', $id )}}" >Lire Plus</a>
           </div>
         </div>

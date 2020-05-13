95f5c47e675eaa41960a8628bc16e63a8c761f61
ihebsaad <ihebsaad@gmail.com>
2020-05-13 18:14:58 +0100

    test

--- resources/views/actus.blade.php
+++ resources/views/actus.blade.php
@@ -262,25 +262,7 @@ $actualites=array();
 <!--/.Carousel Wrapper-->
 
 @section('footer-scripts')
- <script>
- 
-$(' .items').each(function(){
-  var next = $(this).next();
-  if (!next.length) {
-    next = $(this).siblings(':first');
-  }
-  next.children(':first-child').clone().appendTo($(this));
 
-  for (var i=0;i<4;i++) {
-    next=next.next();
-    if (!next.length) {
-      next=$(this).siblings(':first');
-    }
-    next.children(':first-child').clone().appendTo($(this));
-  }
-});
- 
-</script>
 
 @endsection
 

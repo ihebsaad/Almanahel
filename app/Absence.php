076b34e8bc95c5716a733b75da311b215673755a
ihebsaad <ihebsaad@gmail.com>
2020-05-13 10:34:30 +0100

    Absence

---
+++ app/Absence.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+class Actualite extends Model
+
+{
+	
+  protected $fillable = [
+ 'titre',
+ 'contenu',
+ 'image',
+ 'visible',
+ 'extrait',
+ ];
+ 
+}

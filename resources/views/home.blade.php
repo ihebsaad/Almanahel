85537d29ae94eefaa15a84fd60d4d3c183c92368
haythemsahlia <haithemsahlia@gmail.com>
2020-05-14 13:15:46 +0100

    overlay on slider

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,6 +25,15 @@
       width: 100%;
       height: 100%;
   }
+    .carousel-item:after {
+      content:"";
+      position:absolute;
+      top:0;
+      bottom:0;
+      left:0;
+      right:0;
+      background:rgba(0,0,0,0.6);
+    }
   </style>
 
   <header class="masthead" style= "background-image:'<?php echo $link ;?>'" >

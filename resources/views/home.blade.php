c22c203322ffbfae24776378089b582e8940e815
haythemsahlia <haithemsahlia@gmail.com>
2020-05-14 13:24:37 +0100

    slider dark back

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,14 +25,14 @@
       width: 100%;
       height: 100%;
   }
-    #demo > .carousel-item:after {
+    div#demo > div.carousel-inner > div.carousel-item:after {
       content:"";
       position:absolute;
       top:0;
       bottom:0;
       left:0;
       right:0;
-      background:rgba(0,0,0,0.4);
+      background:rgba(0,0,0,0.3);
     }
   </style>
 

479d0a41cb54854015257f2e87c19d6486d7cc48
ihebsaad <ihebsaad@gmail.com>
2020-05-15 13:33:14 +0100

    test

--- resources/views/retards/view.blade.php
+++ resources/views/retards/view.blade.php
@@ -86,6 +86,7 @@
  
 
  @section('footer_scripts')
+    <script src="//cdn.rawgit.com/Eonasdan/bootstrap-datetimepicker/e8bddc60e73c1ec2475f827be36e1957af72e2ea/src/js/bootstrap-datetimepicker.js"></script>
 
  <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
 <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>

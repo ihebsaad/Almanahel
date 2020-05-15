8f2a41ca03bfcb54cb6a587ae6ad345a9f4f5c8b
ihebsaad <ihebsaad@gmail.com>
2020-05-15 13:23:37 +0100

    test

--- resources/views/absences/create.blade.php
+++ resources/views/absences/create.blade.php
@@ -102,7 +102,6 @@
 @section('footer_scripts')
 
 
-    <link rel="stylesheet" type="text/css" media="screen" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" />
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
       <link href="//cdn.rawgit.com/Eonasdan/bootstrap-datetimepicker/e8bddc60e73c1ec2475f827be36e1957af72e2ea/build/css/bootstrap-datetimepicker.css" rel="stylesheet">
  

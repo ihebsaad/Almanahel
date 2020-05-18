b7bc60bda4a94289044877064bec395a34ff0bdd
ihebsaad <ihebsaad@gmail.com>
2020-05-18 11:16:50 +0100

    test

--- resources/views/formation.blade.php
+++ resources/views/formation.blade.php
@@ -14,7 +14,7 @@ $cont =  App\Contenu::where('zone', 'formation')->first();$contenu=$cont->conten
 <section>
 <?php // echo $contenu; ?>
 
-@include('evenements.calendar')
+@include('evenements.annee')
 
  
 </section>

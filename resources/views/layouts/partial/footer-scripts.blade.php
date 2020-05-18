31ec8fd0d04668ed54dc5b27d4c311cbd6396af9
ihebsaad <ihebsaad@gmail.com>
2020-05-18 11:37:24 +0100

    test

--- resources/views/layouts/partial/footer-scripts.blade.php
+++ resources/views/layouts/partial/footer-scripts.blade.php
@@ -10,6 +10,9 @@
   <script     src="{{  URL::asset('public/js/jquery.bootstrap.year.calendar.min.js') }}" ></script>
   
 <script type="text/javascript">var ajax_url = 'https://mdbootstrap.com/wp-load.php';</script><link rel="alternate" hreflang="en" href="https://mdbootstrap.com/plugins/jquery/full-calendar/" />
+<script src="https://cdn.jsdelivr.net/npm/@fullcalendar/core@4.4.0/main.js" type="text/javascript"></script>
+<script src="https://cdn.jsdelivr.net/npm/@fullcalendar/core@4.4.0/locales/fr.js" type="text/javascript"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fullcalendar/core@4.4.0/main.css"  /> 
 
 
 <script  src="{{  URL::asset('public/site/js/jquery.slicknav.min.js') }}"  ></script>

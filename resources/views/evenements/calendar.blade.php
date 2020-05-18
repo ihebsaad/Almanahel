6666cbe5aa543e6bbfc9bee33c482f89596bfa99
ihebsaad <ihebsaad@gmail.com>
2020-05-18 11:32:04 +0100

    test

--- resources/views/evenements/calendar.blade.php
+++ resources/views/evenements/calendar.blade.php
@@ -18,13 +18,16 @@
       plugins: [ 'interaction', 'dayGrid', 'list', 'googleCalendar' ],
 
       header: {
-        left: "Préc,Suiv Aujourd'hui",
+        left: 'prev,next today',
         center: 'title',
         right: 'dayGridMonth,listYear'
       },
 
       displayEventTime: true, // don't show the time column in list view
-
+  defaultDate: '<?php echo date('Y-m-d');?>',
+    navLinks: true,
+    eventLimit: true,
+  
       // THIS KEY WON'T WORK IN PRODUCTION!!!
       // To make your own Google API key, follow the directions here:
       // http://fullcalendar.io/docs/google_calendar/
@@ -32,7 +35,37 @@
 
       // TN Holidays
        events: 'ar.tn#holiday@group.v.calendar.google.com',
-
+events: [{
+            title: 'Front-End Conference',
+            start: '2018-11-16',
+            end: '2018-11-18'
+        },
+        {
+            title: 'Hair stylist with Mike',
+            start: '2018-11-20',
+            allDay: true
+        },
+        {
+            title: 'Car mechanic',
+            start: '2018-11-14T09:00:00',
+            end: '2018-11-14T11:00:00'
+        },
+        {
+            title: 'Dinner with Mike',
+            start: '2018-11-21T19:00:00',
+            end: '2018-11-21T22:00:00'
+        },
+        {
+            title: 'Chillout',
+            start: '2018-11-15',
+            allDay: true
+        },
+        {
+            title: 'Vacation',
+            start: '2018-11-23',
+            end: '2018-11-29'
+        },
+    ]
       eventClick: function(arg) {
         // opens events in a popup window
         window.open(arg.event.url, 'google-calendar-event', 'width=700,height=600');

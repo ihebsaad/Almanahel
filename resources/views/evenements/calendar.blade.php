04fbbc87bc0032d951636d99267fe7022b5d08cb
ihebsaad <ihebsaad@gmail.com>
2020-05-18 10:30:50 +0100

    test calendar

---
+++ resources/views/evenements/calendar.blade.php
@@ -0,0 +1,86 @@
+
+@extends('layouts.front')
+
+    <!-- Bootstrap CSS  
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- Javascripts  
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+            integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+            crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+            integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+            crossorigin="anonymous"></script>
+    ------->
+ <script  src="{{ asset('public/js/jquery.bootstrap.year.calendar.js') }}"  type="text/javascript"></script>
+ <link rel="stylesheet" href="{{ asset('public/css/jquery.bootstrap.year.calendar.css') }}"   > 
+
+@section('content')
+Calendrier Général
+<div class="container">
+            <div class="calendar"></div>
+</div>
+@endsection
+
+
+ @section('footer-scripts')
+  
+  <script>
+  
+  
+      $('.calendar').calendar();
+
+/*
+    $(document).ready(function(){
+
+    $('#calendar').fullCalendar({
+    header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'month,agendaWeek,agendaDay,listWeek'
+    },
+     defaultDate: '2018-11-16',
+   // defaultDate: '<?php echo date('Y-m-d');?>',
+    navLinks: true,
+    eventLimit: true,
+    events: [{
+            title: 'Front-End Conference',
+            start: '2018-11-16',
+            end: '2018-11-18'
+        },
+        {
+            title: 'Hair stylist with Mike',
+            start: '2018-11-20',
+            allDay: true
+        },
+        {
+            title: 'Car mechanic',
+            start: '2018-11-14T09:00:00',
+            end: '2018-11-14T11:00:00'
+        },
+        {
+            title: 'Dinner with Mike',
+            start: '2018-11-21T19:00:00',
+            end: '2018-11-21T22:00:00'
+        },
+        {
+            title: 'Chillout',
+            start: '2018-11-15',
+            allDay: true
+        },
+        {
+            title: 'Vacation',
+            start: '2018-11-23',
+            end: '2018-11-29'
+        },
+    ]
+});     
+
+    });
+	
+	*/
+</script>
+@endsection
+
+

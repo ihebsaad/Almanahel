65ba47bc1d65ad6ddca5719efa793f51cbee47a7
haythemsahlia <haithemsahlia@gmail.com>
2020-05-18 17:59:46 +0100

    exception inscrit sans login

--- app/Http/Controllers/InscriptionsController.php
+++ app/Http/Controllers/InscriptionsController.php
@@ -17,7 +17,7 @@ class InscriptionsController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth', ['except' => ['store']]);
     }
 
     /**

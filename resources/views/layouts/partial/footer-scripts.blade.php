57e5555333d19c8a3e9032b5c68eb3f016f8be7a
ihebsaad <ihebsaad@gmail.com>
2020-05-18 12:03:19 +0100

    test

--- resources/views/layouts/partial/footer-scripts.blade.php
+++ resources/views/layouts/partial/footer-scripts.blade.php
@@ -14,6 +14,50 @@
   
   $(document).ready(function(){
 
+ $('#calendar').fullCalendar({
+    header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'month,agendaWeek,agendaDay,listWeek'
+    },
+     defaultDate: '2018-11-16',
+   // defaultDate: '<?php echo date('Y-m-d');?>',
+    navLinks: true,
+    eventLimit: true,
+    events: [{
+            title: 'Front-End Conference',
+            start: '2018-11-16',
+            end: '2018-11-18'
+        },
+        {
+            title: 'Hair stylist with Mike',
+            start: '2018-11-20',
+            allDay: true
+        },
+        {
+            title: 'Car mechanic',
+            start: '2018-11-14T09:00:00',
+            end: '2018-11-14T11:00:00'
+        },
+        {
+            title: 'Dinner with Mike',
+            start: '2018-11-21T19:00:00',
+            end: '2018-11-21T22:00:00'
+        },
+        {
+            title: 'Chillout',
+            start: '2018-11-15',
+            allDay: true
+        },
+        {
+            title: 'Vacation',
+            start: '2018-11-23',
+            end: '2018-11-29'
+        },
+    ]
+});     
+
+
     $("select#elevestat").change(function(){
         $(this).find("option:selected").each(function(){
             var optionValue = $(this).attr("value");

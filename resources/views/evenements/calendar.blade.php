0e1d723bfd5415bec5cfe6493920a405a4704237
ihebsaad <ihebsaad@gmail.com>
2020-05-18 11:51:32 +0100

    test

--- resources/views/evenements/calendar.blade.php
+++ resources/views/evenements/calendar.blade.php
@@ -27,8 +27,7 @@
       displayEventTime: true, // don't show the time column in list view
   defaultDate: '<?php echo date('Y-m-d');?>',
     navLinks: true,
-    eventLimit: true,
-  
+   
       // THIS KEY WON'T WORK IN PRODUCTION!!!
       // To make your own Google API key, follow the directions here:
       // http://fullcalendar.io/docs/google_calendar/
@@ -39,56 +38,61 @@
 	   businessHours: true, // display business hours
        events: [
         {
-          title: 'Business Lunch',
-          start: '2020-02-03T13:00:00',
-          constraint: 'businessHours'
+          title: 'All Day Event',
+          start: '2020-02-01'
         },
         {
-          title: 'Meeting',
-          start: '2020-02-13T11:00:00',
-          constraint: 'availableForMeeting', // defined below
-          color: '#257e4a'
+          title: 'Long Event',
+          start: '2020-02-07',
+          end: '2020-02-10'
+        },
+        {
+          groupId: 999,
+          title: 'Repeating Event',
+          start: '2020-02-09T16:00:00'
+        },
+        {
+          groupId: 999,
+          title: 'Repeating Event',
+          start: '2020-02-16T16:00:00'
         },
         {
           title: 'Conference',
-          start: '2020-02-18',
-          end: '2020-02-20'
+          start: '2020-02-11',
+          end: '2020-02-13'
         },
         {
-          title: 'Party',
-          start: '2020-02-29T20:00:00'
-        },
-
-        // areas where "Meeting" must be dropped
-        {
-          groupId: 'availableForMeeting',
-          start: '2020-02-11T10:00:00',
-          end: '2020-02-11T16:00:00',
-          rendering: 'background'
+          title: 'Meeting',
+          start: '2020-02-12T10:30:00',
+          end: '2020-02-12T12:30:00'
         },
         {
-          groupId: 'availableForMeeting',
-          start: '2020-02-13T10:00:00',
-          end: '2020-02-13T16:00:00',
-          rendering: 'background'
-        },
-
-        // red areas where no events can be dropped
-        {
-          start: '2020-02-24',
-          end: '2020-02-28',
-          overlap: false,
-          rendering: 'background',
-          color: '#ff9f89'
+          title: 'Lunch',
+          start: '2020-02-12T12:00:00'
         },
         {
-          start: '2020-02-06',
-          end: '2020-02-08',
-          overlap: false,
-          rendering: 'background',
-          color: '#ff9f89'
+          title: 'Meeting',
+          start: '2020-02-12T14:30:00'
+        },
+        {
+          title: 'Happy Hour',
+          start: '2020-02-12T17:30:00'
+        },
+        {
+          title: 'Dinner',
+          start: '2020-02-12T20:00:00'
+        },
+        {
+          title: 'Birthday Party',
+          start: '2020-02-13T07:00:00'
+        },
+        {
+          title: 'Click for Google',
+          url: 'http://google.com/',
+          start: '2020-02-28'
         }
       ],
+    
       eventClick: function(arg) {
         // opens events in a popup window
         window.open(arg.event.url, 'google-calendar-event', 'width=700,height=600');

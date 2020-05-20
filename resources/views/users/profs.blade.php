8a96af832cca32e91d062184ec2c20ff3b1147a0
ihebsaad <ihebsaad@gmail.com>
2020-05-20 10:13:58 +0100

    test

--- resources/views/users/profs.blade.php
+++ resources/views/users/profs.blade.php
@@ -4,8 +4,7 @@
 <link rel="stylesheet" type="text/css" href="{{ asset('resources/assets/datatables/css/buttons.bootstrap.css') }}" />
 <link rel="stylesheet" type="text/css" href="{{ asset('resources/assets/datatables/css/scroller.bootstrap.css') }}" />
  
-   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"  >
- 
+  
  <?php
 
 

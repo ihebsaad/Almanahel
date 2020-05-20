587446233d6d939f864115b2f655ed74ac7bed41
ihebsaad <ihebsaad@gmail.com>
2020-05-20 10:11:23 +0100

    test

--- resources/views/users/profs.blade.php
+++ resources/views/users/profs.blade.php
@@ -1,13 +1,11 @@
 @extends('layouts.back')
-<!--
+ 
 <link rel="stylesheet" type="text/css" href="{{ asset('resources/assets/datatables/css/dataTables.bootstrap.css') }}" />
 <link rel="stylesheet" type="text/css" href="{{ asset('resources/assets/datatables/css/buttons.bootstrap.css') }}" />
 <link rel="stylesheet" type="text/css" href="{{ asset('resources/assets/datatables/css/scroller.bootstrap.css') }}" />
  
-
- -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"  >
-
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"  >
+ 
  <?php
 
 

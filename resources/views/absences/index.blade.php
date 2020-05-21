dc4e69c01e8b760e03ca99dc04616de76fd84568
ihebsaad <ihebsaad@gmail.com>
2020-05-21 10:52:38 +0100

    test

--- resources/views/absences/index.blade.php
+++ resources/views/absences/index.blade.php
@@ -50,7 +50,7 @@
 				?>
                 <tr>
                     <td style="width:5%" ><a href="{{action('AbsencesController@view', $absence['id'])}}" >{{$absence->id}}</a></td>
-                    <td style="width:20%" ><?php  echo $classe->titre ; ?> </td>
+                    <td style="width:20%" ><?php // echo $classe->titre ; ?> </td>
                     <td style="width:20%" ><?php echo $user->name .' '.$user->lastname;;?> </td>
                     <td style="width:10%" ><?php echo $absence['debut'];?> </td>
                     <td style="width:10%" ><?php echo $absence['fin'];?> </td>

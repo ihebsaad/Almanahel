4f0c60d609e2c1aad0c367e84f6d11fc96ac16fa
ihebsaad <ihebsaad@gmail.com>
2020-05-22 12:55:06 +0100

    test

--- resources/views/absences/view.blade.php
+++ resources/views/absences/view.blade.php
@@ -98,17 +98,7 @@
  
 
 @section('footer_scripts')
-    <link rel="stylesheet" type="text/css" media="screen" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-      <link href="//cdn.rawgit.com/Eonasdan/bootstrap-datetimepicker/e8bddc60e73c1ec2475f827be36e1957af72e2ea/build/css/bootstrap-datetimepicker.css" rel="stylesheet">
- 
-    <script type="text/javascript" src="//code.jquery.com/jquery-2.1.1.min.js"></script>
-
-    <script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment-with-locales.js"></script>
-
-
-    <script src="//cdn.rawgit.com/Eonasdan/bootstrap-datetimepicker/e8bddc60e73c1ec2475f827be36e1957af72e2ea/src/js/bootstrap-datetimepicker.js"></script>
-
+     
 
  <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
 <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
@@ -124,14 +114,7 @@ function toggle(className, displayState){
   }
 
 $(function () {
-	
-     $('#datetimepicker1').datetimepicker({
-                    locale: 'fr'
-                });
- 	
-     $('#datetimepicker2').datetimepicker({
-                    locale: 'fr'
-                });
+	 
  			
 $('.select2').select2({
 filter: true,

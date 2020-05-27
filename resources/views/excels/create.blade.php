b228a86e12adb470faf629a6432a73185ad82255
ihebsaad <ihebsaad@gmail.com>
2020-05-27 10:55:33 +0100

    test

--- resources/views/excels/create.blade.php
+++ resources/views/excels/create.blade.php
@@ -41,19 +41,19 @@
                          <label for="name" class="col-md-4 control-label">Mois</label>
 						<?php $mois=0;?>
 					<select     class="form-control input"   name="mois" id="mois"    style="width:150px">
-                        <option value=""></option>
-                        <option    >  Janvier  </option>
-                        <option   >   Février  </option>
-                        <option    >  Mars  </option>
-                        <option    >  Avril  </option>
-                        <option    >  Mai  </option>
-                        <option    >  Juin  </option>
-                        <option   >  Juillet  </option>
-                        <option    >  Août  </option>
-                        <option    >  Septembre  </option>
-                        <option    >  Octobre  </option>
-                        <option   >  Novembre  </option>
-                        <option    >  Décembre  </option>
+                        <option value="0"></option>
+                        <option  value="1"  >  Janvier  </option>
+                        <option  value="2" >  Février  </option>
+                        <option  value="3"  >  Mars  </option>
+                        <option  value="4"  >  Avril  </option>
+                        <option  value="5"  >  Mai  </option>
+                        <option  value="6"  >  Juin  </option>
+                        <option  value="7" >  Juillet  </option>
+                        <option  value="8"  >  Août  </option>
+                        <option  value="9"  >  Septembre  </option>
+                        <option  value="10"  >  Octobre  </option>
+                        <option  value="11" >  Novembre  </option>
+                        <option  value="12"  >  Décembre  </option>
                     </select>
                         </div>
                      

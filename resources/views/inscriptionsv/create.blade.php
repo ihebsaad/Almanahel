2914d0edfdbe703e5dfa98d37a7562b4d8a04ba6
ihebsaad <ihebsaad@gmail.com>
2020-05-31 13:47:23 +0100

    test

--- resources/views/inscriptionsv/create.blade.php
+++ resources/views/inscriptionsv/create.blade.php
@@ -107,17 +107,15 @@
 </style>
 
 <h3 style="margin-left:50px">Créer une inscription </h3><br><br>    
-           <div class="row"  style="margin-left:20px">
+  <div class="row"  style="margin-left:20px">
   <section  style=" font-size: 18px; font-weight: 1300; color: #545454;">
 Inscription pour : <select id="elevestat" onBlur="eleveverif()" style="width:1100" class="form-control">
             <option>Selectionner</option>
-            <option value="nouveau">Nouveau</option>
-            <option value="ancien">Ancien</option>
+            <option value="nouveau">Nouveau élève</option>
+            <option value="ancien">Ancien élève</option>
         </select>
-         élève.
   </section>
 </div>
-<br><br> <br><br> <br><br> 
 <div id="ancien_box" style="display:none">
    <form class="form-horizontal" method="POST" action="{{ route('inscriptionsv.inscriptionsadd') }}">
       {{ csrf_field() }}
@@ -128,39 +126,25 @@ Inscription pour : <select id="elevestat" onBlur="eleveverif()" style="width:110
 		$annee=intval($year);
 		if($mois > 9 ){$annee=$annee-1;}
 		?>
-	  <input id="annee" type="hidden" class="form-control" name="annee"  value="<?php echo $annee;?>"/>
-			   
-			   
-<div class="row" style="margin-left:20px">
-  <div class="form-group " >
-    <div class=" row">
-                                   
-    Sélectionnez l'élève <br> <br>                                
- <select style="width:1300" class="form-control select2"  id="champ1" name="champ1"  onchange="checkexiste1(this)"   >
-                       
-                  
-                       
+ <input id="annee" type="hidden" class="form-control" name="annee"  value="<?php echo $annee;?>"/>
+ 
+   <div class="form-group " >
+                                    
+    Sélectionnez l'élève <br> $                                
+ <select style="width:1300" class="form-control select2"  id="champ1" name="champ1"  onchange="checkexiste1(this)"   >      
 @foreach($users as $user)
                             <option value="<?php  echo $user->id; ?>"><?php echo $user->lastname .$user->name; ?></option>                          
                              @endforeach
-                       
-
-                     </select>
-
-                                    </div>
-                                        </div>
-                                            </div>
-                                
-
-<br><br> <br><br> <br><br> 
-
+   </select>
+    </div>
+  
     <div class="row form-group" style="margin-bottom:30px">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit"  id="test" class="btn btn-primary">
                                    S'inscrire
                                 </button>
                             </div>
-                        </div>
+ </div>
 </form>  
 
                          </div>
@@ -172,9 +156,9 @@ Inscription pour : <select id="elevestat" onBlur="eleveverif()" style="width:110
 <div class="row" style="margin-left:20px">
   <div class="form-group " >
     <div class=" row">
-                            Sélectionnez l'élève   <br> <br>     
+   Sélectionnez l'élève   <br>     
                                     
-             <select  style="width:1300" class="form-control select2"  id="champ2" name="champ2"  onchange="checkexiste2(this)"  >
+    <select  style="width:1300" class="form-control select2"  id="champ2" name="champ2"  onchange="checkexiste2(this)"  >
                        
                   
                        

6777d326efbe1fbf82657f09a5c1540785d888e9
ihebsaad <ihebsaad@gmail.com>
2020-06-01 11:14:42 +0100

    test

--- resources/views/excels/view.blade.php
+++ resources/views/excels/view.blade.php
@@ -27,7 +27,7 @@
                             <div class="col-md-6">
                                 <input  autocomplete="off"  id="chemin" type="file" class="form-control" name="chemin"  required   accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel" >
  
-                                         <a class="form-control" href="http://<?php echo $_SERVER['HTTP_HOST'];?>/storage/documents/<?php echo $excel['chemin'];?>" > 
+                                         <a class="form-control" href="http://<?php echo $_SERVER['HTTP_HOST'];?>/storage/excels/<?php echo $excel['chemin'];?>" > 
            <span class="fa fa-fw fa-download-alt"></span> <?php echo $excel['chemin'];?>
 			</a>
                             </div>

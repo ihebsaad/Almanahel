b78d45b8549b48f0908927dc7dabe358f6b1344e
ihebsaad <ihebsaad@gmail.com>
2020-06-05 09:52:36 +0100

    test

--- resources/views/envoyes/send.blade.php
+++ resources/views/envoyes/send.blade.php
@@ -67,8 +67,7 @@
                             <select id="attachs"  class="itemName form-control col-lg-12" style="" name="attachs[]"  multiple  value="$('#attachs').val()">
                                 <option></option>
                                 @foreach($attachements as $attach)
-                               <!--     <option  value="<?php echo $attach->id;?>" pathem="<?php  echo URL::asset('storage'.addslashes($attach->path)) ; ?>" typeem="<?php echo $attach->type;?>"><?php echo $attach->nom;?> </option>-->
-                                     <option value="<?php   echo $attach->id;?>"> <?php  echo $attach->nom;?> </option>
+                                      <option value="<?php   echo $attach->id;?>"> <?php  echo $attach->titre;?> </option>
                                 @endforeach
                             </select>
                         </div>

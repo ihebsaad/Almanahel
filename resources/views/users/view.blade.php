4b80f3f26e07bca6acddbe50280b6ea6ffd87682
ihebsaad <ihebsaad@gmail.com>
2020-06-09 12:02:58 +0100

    test

--- resources/views/users/view.blade.php
+++ resources/views/users/view.blade.php
@@ -65,6 +65,7 @@
                      <option value="prof"  <?php if($user->user_type=='prof') {echo'selected="selected"';}?> >Enseignant </option>
                     <option  value="parent"  <?php if($user->user_type=='parent') {echo'selected="selected"';}?>  >Parent</option>
                     <option  value="membre"  <?php if($user->user_type=='membre') {echo'selected="selected"';}?>  >Membre d'administration</option>
+                    <option  value="financier"  <?php if($user->user_type=='financier') {echo'selected="selected"';}?>  >Finances</option>
                     <option  value="conseil"  <?php if($user->user_type=='conseil') {echo'selected="selected"';}?>  >Conseil de pilotage</option>
                     <option  value="suivi"  <?php if($user->user_type=='suivi') {echo'selected="selected"';}?>  >Suivi pédagogique</option>
                    

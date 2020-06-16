294a827e0be9081b7d1f21f6c1192f808c96690a
iheb <ihebsaad@gmail.com>
2020-06-16 11:07:20 +0100

    config

--- config/mail.php
+++ config/mail.php
@@ -57,7 +57,7 @@ return [
 
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'lyceealmanahel@gmail.com'),
-        'name' => env('MAIL_FROM_NAME', 'Lycee Almanahel'),
+        'name' => env('MAIL_FROM_NAME', 'Lycée Almanahel'),
     ],
 
     /*

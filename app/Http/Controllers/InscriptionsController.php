f714cf3d7e82aaa3a7ed09d06f046d4d1ddbf3f8
hammalisirine <hammalisirine120@gmail.com>
2020-07-22 01:09:58 -0700

    correction

--- app/Http/Controllers/InscriptionsController.php
+++ app/Http/Controllers/InscriptionsController.php
@@ -18,7 +18,7 @@ class InscriptionsController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth', ['except' => ['store','checkexiste2']]);
+        $this->middleware('auth', ['except' => ['store'],'except' => ['checkexiste2']]);
     }
 
     /**

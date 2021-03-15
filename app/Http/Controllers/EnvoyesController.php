59bb4716b516a64ab060edd7eaa2ae7df090aa7f
hammali sirine <hammalisirine120@gmail.com>
2021-03-15 11:55:49 +0100

    modification port ionos

--- app/Http/Controllers/EnvoyesController.php
+++ app/Http/Controllers/EnvoyesController.php
@@ -355,7 +355,7 @@ $user = auth()->user();
 		$id=Auth::id();
 	 } else{$id=0;}
 
- $swiftTransport =  new \Swift_SmtpTransport( 'smtp.ionos.fr', '25', 'tls');
+ $swiftTransport =  new \Swift_SmtpTransport( 'smtp.ionos.fr', '587', 'tls');
         $swiftTransport->setUsername('contact@lyceealmanahel.com'); //adresse email
         $swiftTransport->setPassword('Almanahel2020!'); // mot de passe email
 		
